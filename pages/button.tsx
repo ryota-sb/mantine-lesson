0d1f5ee809888f23a4dd547234b17bff23d78b73
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-15 18:30:37 +0900

    button component

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
+import { ReplyIcon } from "@heroicons/react/solid";
 import { Button, Group } from "@mantine/core";
 import { Layout } from "../components/Layout";
+import { BrandGithub, ThreeDCubeSphere } from "tabler-icons-react";
 
 const ButtonDemo = () => {
   return (
     <Layout title="Button">
-      <Group>
-        <Button>Press</Button>
+      <Group position="center">
+        <Button color="green" radius="lg" uppercase leftIcon={<BrandGithub />}>
+          Sign up
+        </Button>
+        <Button mt="md" leftIcon={<ThreeDCubeSphere />}>
+          Sign in
+        </Button>
+        <Link href="/">
+          <ReplyIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
+        </Link>
       </Group>
     </Layout>
   );
