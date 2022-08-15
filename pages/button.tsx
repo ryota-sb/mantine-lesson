9cca90fac6a420506367516375d607d5d81f77a6
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-15 13:01:45 +0900

    layout 修正

---
+++ pages/button.tsx
@@ -0,0 +1,14 @@
+import { Button, Group } from "@mantine/core";
+import { Layout } from "../components/Layout";
+
+const ButtonDemo = () => {
+  return (
+    <Layout title="Button">
+      <Group>
+        <Button>Press</Button>
+      </Group>
+    </Layout>
+  );
+};
+
+export default ButtonDemo;
