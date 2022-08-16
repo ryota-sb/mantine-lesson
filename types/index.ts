bbbfb3fadc538dcdf7320e1ee311a90121fa4f3c
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-17 03:23:41 +0900

    add post type

---
+++ types/index.ts
@@ -0,0 +1,14 @@
+export type Form = {
+  email: string;
+  password: string;
+  age: number;
+};
+
+export type Post = {
+  id: number;
+  created_at: string;
+  title: string;
+  content: string;
+  post_url: string;
+  status: string;
+};
